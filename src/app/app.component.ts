ef28f1e901d6fc5d7a1573a932b59f7645451fb1
MarionLeHerisson <marion.hurteau1@gmail.com>
2020-05-12 10:58:42 +0200

    add 3000

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'POKEMON XTREM BATTLE SIMULATOR';
+  title = 'POKEMON XTREM BATTLE SIMULATOR 3000';
 }
